165f3b0d48f5991b6333ed7ce99594aeef56cb5c
unknown <rafiadnan200508@gmail.com>
2026-03-31 13:30:50 +0800

    update terbaru 31 maret

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'http://192.168.1.16:8000/api'
+    baseURL: 'http://192.168.1.30:8000/api'
 
 })
 api.interceptors.request.use((config) => {
@@ -11,4 +11,4 @@ api.interceptors.request.use((config) => {
     return config
 });
 
-export default api
+export default api;
